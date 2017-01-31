06c8ed2de70a2179559a53fd13b289bf1edc929a
Valters Jansons <valter.jansons@gmail.com>
2017-01-31 21:22:59 +0200

    Redirect the disabled pages to the log-in
    We do not want people seeing the disabled pages when
    they are not logged in. Security and housekeeping.

--- server.js
+++ server.js
@@ -248,6 +248,21 @@ i18n.getLocales().forEach((locale) => {
 const catchAll = (localeHash, locale, view, title, renderOverrides) => {
   app.get(encodeURI(localeHash[locale].toLowerCase().split(' ').join('-')), (req, res) => {
     req.setLocale(locale);
+    if ([ 'Survival Guide', 'Participation', 'Donation' ].indexOf(title) !== -1 && !res.locals.user) {
+      const target = '/' + locale + '/' + req.__('Log In').toLowerCase().split(' ').join('-');
+      res.render('redirect', { target: target }, (err, html) => {
+        res.status(302);
+        res.location(target);
+        if (err) {
+          res.type('text/plain; charset=utf-8');
+          res.send(target);
+          console.error(err.stack);
+        } else {
+          res.type('text/html; charset=utf-8');
+          res.send(html);
+        }
+      });
+    }
     const render = (markdown) => {
       const renderParams = Object.assign({ altLocales: localeHash, title: req.__(title), markdown: markdown }, renderOverrides || {});
       res.render(view.split('.')[0], renderParams, (err, html) => {
